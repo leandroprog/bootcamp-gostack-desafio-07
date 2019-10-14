d5dd6b5d120c3b4787e13ef0e93d0642a45ded9e
Leandro Rocha <prog.leandro@gmail.com>
2019-10-13 23:50:58 -0300

    Configurando o ambiente

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,48 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {FlatList, View, Text} from 'react-native';
+import {MdShoppingCart} from 'react-icons/md';
+import api from '../../services/api';
+// import formatPrice from '../../util/format';
+
+import {Container} from './styles';
 
 export default class Home extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    console.tron.log('leo', response);
+
+    const data = response.data.map(product => ({
+      ...product,
+      // priceFormatted: formatPrice(product.price),
+    }));
+
+    // eslint-disable-next-line react/no-unused-state
+    this.setState({products: data});
+  }
+
   render() {
+    const {products} = this.state;
+
+    console.tron.log('ss', products);
+
     return (
-      <>
-        <View style={styles.container}>
-          <Text>HOME</Text>
-        </View>
-      </>
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          renderItem={({item}) => (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ src/pages/home/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import {darken} from 'polished';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  background: #fff;
+`;
+
+// export const Product = styled.View`
+//   background: #fff;
+//   padding: 10px;
+//   margin: 15px;
+//   border-radius: 4px;
+//   width: 220px;
+// `;
